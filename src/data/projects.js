3a9a5e7f7916886c808086435bc05e9c9b32b8ba
Angelo Bongiorno <s331164@studenti.polito.it>
2026-04-24 13:29:17 +0200

    added sections

---
+++ src/data/projects.js
@@ -0,0 +1,63 @@
+export const projects = [
+    {
+        id: 1,
+        tag: "domain specific chatbot",
+        title: "AI-based Team Builder",
+        description: "A chatbot interface that helps building competitive Pokémon teams.",
+        additionalInfo: ["Complete with a custom knowledge base and external tools integration", "Leverages LLMs, RAG and usage of external tools"], 
+        tech: ["Python", "OpenRouterAPI"],
+        status: "In progress",
+        link: "https://github.com/AngeloBongiorno/vgc-team-builder"
+    },
+    {
+        id: 2,
+        tag: "local search engine",
+        title: "tRUSTami",
+        description: "A CLI-based local document search engine for XML documents.",
+        additionalInfo: ["Leverages TF-IDF", "Built with Rust for performance and safety"],
+        tech: ["Rust"],
+        status: "In progress",
+        link: "https://github.com/AngeloBongiorno/trustami"
+    },
+    /*
+    {
+        id: 3,
+        tag: "domain specific chatbot",
+        title: "AI-based Team Builder",
+        description: "A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools. \
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools \
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools.\
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools.",
+        tech: ["Python", "OpenRouterAPI"],
+        status: "In progress",
+        link: "https://github.com/AngeloBongiorno/vgc-team-builder"
+    },
+
+
+    {
+        id: 4,
+        tag: "domain specific chatbot",
+        title: "AI-based Team Builder",
+        description: "A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools. \
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools \
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools.\
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools.",
+        tech: ["Python", "OpenRouterAPI"],
+        status: "In progress",
+        link: "https://github.com/AngeloBongiorno/vgc-team-builder"
+    },
+
+    {
+        id: 5,
+        tag: "domain specific chatbot",
+        title: "AI-based Team Builder",
+        description: "A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools. \
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools \
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools.\
+        A chatbot interface that helps building competitive Pokémon teams, leveraging LLMs, RAG and usage of external tools.",
+        tech: ["Python", "OpenRouterAPI"],
+        status: "In progress",
+        link: "https://github.com/AngeloBongiorno/vgc-team-builder"
+    },
+*/
+]
