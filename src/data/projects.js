96650d74344e9d0b02d64bc3c4488cc72d21c1fe
Angelo Bongiorno <s331164@studenti.polito.it>
2026-04-25 11:40:13 +0200

    contents updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,6 +19,26 @@ export const projects = [
         status: "In progress",
         link: "https://github.com/AngeloBongiorno/trustami"
     },
+    {
+        id: 3,
+        tag: "multi-agent chatbot",
+        title: "Multi-agent chatbot for the automotive domain",
+        description: "A multi-agent chatbot designed for managing complex requests in the automotive domain.",
+        additionalInfo: ["Helps navigating a complex knowledge base through natural language queries", "Leverages hierarchical multi-agent collaboration", "Developed as part of my Master's thesis"],
+        tech: ["Python", "LangChain", "Azure Cloud"],
+        status: "Completed",
+        link: null
+    },
+    {
+        id: 4,
+        tag: "image domain adaptation study",
+        title: "Real-time domain Adaptation for Semantic Segmentation",
+        description: "Implemented and compared various domain adaptation techniques for the PID-net real time segmentation network.",
+        additionalInfo: ["Experimented with image augmentation, adversarial training, cross-domain mixed sampling and image-to-image translation", "Google Earth dataset for training and evaluation", "Developed as part of one of my Master's courses"],
+        tech: ["Python", "PyTorch", "Jupyter"],
+        status: "Completed",
+        link: "https://github.com/AngeloBongiorno/AML_2025_project4"
+    }
     /*
     {
         id: 3,
